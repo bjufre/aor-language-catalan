ca5a9d060ec8e0cc0203cbe4c9a06fa36ca98cf5
Bernat Jufré <info@behind.design>
2017-11-23 14:53:29 +0100

    Add Catalan translation

---
+++ README.md
@@ -0,0 +1,34 @@
+# AOR Language Catalan
+> Catalan translations for [admin-on-rest](https://github.com/marmelab/admin-on-rest), the frontend framework for building admin applications on top of REST services.
+
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)]()
+[![npm](https://img.shields.io/npm/v/aor-language-catalan.svg)]()
+<!-- [![npm](https://img.shields.io/npm/dm/aor-language-catalan.svg)]() -->
+
+### Installation
+
+##### NPM
+```bash
+npm install --save aor-language-catalan
+```
+
+##### YARN
+```bash
+yarn add aor-language-catalan
+```
+
+### Usage
+```js
+import catalanMessages from 'aor-language-catalan';
+
+const messages = {
+  ca: catalanMessages
+};
+
+<Admin locale="ca" messages={messages}>
+  ...
+</Admin>
+```
+
+### License
+This translation is licensed under the [MIT Licence](https://github.com/behind-design/aor-language-catalan/blob/master/LICENSE), and sponsored by [Behind Design](https://github.com/behind-design).
---
+++ index.js
@@ -0,0 +1,67 @@
+module.exports = {
+  aor: {
+    action: {
+      delete: "Eliminar",
+      show: "Mostrar",
+      list: "Llistar",
+      save: "Guardar",
+      cancel: "Cancel·lar",
+      create: "Crear",
+      edit: "Editar",
+      refresh: "Actualizar",
+      add_filter: "Afegir filtre",
+      remove_filter: "Eliminar filtre"
+    },
+    boolean: {
+      true: "Si",
+      false: "No"
+    },
+    page: {
+      list: "Llistar %{name}",
+      edit: "%{name} #%{id}",
+      show: "%{name} #%{id}",
+      create: "Crear %{name}",
+      delete: "Eliminar %{name} #%{id}",
+      dashboard: "Panell de control"
+    },
+    input: {
+      image: {
+        upload_several:
+          "Deixi anar alguns arxius per carregar o faci clic per seleccionar un.",
+        upload_single:
+          "Deixi anar un arxiu per carregar-lo o faci clic per seleccionar-lo."
+      }
+    },
+    message: {
+      yes: "Si",
+      no: "No",
+      are_you_sure: "Estàs segur?",
+      about: "Sobre"
+    },
+    navigation: {
+      no_results: "No s'han trovat resultats",
+      page_out_of_boundaries: "La pàgina %{page} està fora del límit",
+      page_out_from_end: "No es pot anar més enllà de l'última pàgina",
+      page_out_from_begin: "No es pot anar abans de la pàgina 1",
+      page_range_info: "%{offsetBegin}-%{offsetEnd} de %{total}",
+      next: "Següent",
+      prev: "Anterior"
+    },
+    auth: {
+      username: "Usuari",
+      password: "Contrasenya",
+      sign_in: "Accedir",
+      sign_in_error: "S'ha produït un error d'autenticació.",
+      logout: "Tancar sessió"
+    },
+    notification: {
+      updated: "Element actualitzat",
+      created: "Element creat",
+      deleted: "Element eliminat",
+      item_doesnt_exist: "L'element no existeix"
+    },
+    validation: {
+      required: "Obligatori"
+    }
+  }
+};
